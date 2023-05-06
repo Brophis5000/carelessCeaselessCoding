f01d41a7c50012db97fd37b2ce5fe9eb3f362c45
Brophis5000 <joseph.kusar@gmail.com>
2023-05-05 21:21:14 -0400

    house

--- Application/Main.java
+++ Application/Main.java
@@ -2,6 +2,6 @@ import javax.imageio.ImageIO;
 
 public class Main{
     public static void main(String[] args){
-        System.out.print("Allen said, \"I like sucking little semicolons.\"");
+        System.out.print("  /\\\n /  \\\n |  |\n |  |\n ----");
     }
 }
