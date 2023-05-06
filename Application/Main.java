803f3cc8be873323fe40df40d9f96cf2147906c1
Brophis5000 <joseph.kusar@gmail.com>
2023-05-05 21:42:45 -0400

    Schedule

--- Application/Main.java
+++ Application/Main.java
@@ -2,6 +2,15 @@ import javax.imageio.ImageIO;
 
 public class Main{
     public static void main(String[] args){
-        System.out.print("  /\\\n /  \\\n |  |\n |  |\n ----");
+        System.out.printf("%-40s %20s", " 1. AWBCC-Computer SYS- Cyber Security", "Zottoli, Damien\n");
+        System.out.printf("%-40s %20s", " 2. AWBCC-Computer SYS- Cyber Security", "Zottoli, Damien\n");
+        System.out.printf("%-40s %20s", " 3. AWBCC-Computer SYS- Cyber Security", "Zottoli, Damien\n");
+        System.out.printf("%-40s %20s", " 4. AWBCC-Computer SYS- Cyber Security", "Zottoli, Damien\n");
+        System.out.printf("%-40s %18s", " 5. Academic Resource", "Kilroy, Alice\n");
+        System.out.printf("%-40s %21s", " 6. Conceptual Chemistry", "Stright, Jeanine\n");
+        System.out.printf("%-40s %24s", " 7. Geometry", "Pedrosky, Alexandra\n");
+        System.out.printf("%-40s %18s", " 8. American Government", "Pfab, Brandon\n");
+        System.out.printf("%-40s %20s", " 9. American Literature", "Byrnes, Margret\n");
+        System.out.printf("%-40s %18s", " VL. PF: Sports & Rec", "Jones, Vertus\n");
     }
 }
