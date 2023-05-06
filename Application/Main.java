32a8e21014b101a1846dabd24e7350f29777a10e
Brophis5000 <joseph.kusar@gmail.com>
2023-05-05 21:13:03 -0400

    altered output

--- Application/Main.java
+++ Application/Main.java
@@ -2,6 +2,6 @@ import javax.imageio.ImageIO;
 
 public class Main{
     public static void main(String[] args){
-        System.out.print("Hello World!");
+        System.out.print("Allen said, \"I like sucking little semicolons.\"");
     }
 }
